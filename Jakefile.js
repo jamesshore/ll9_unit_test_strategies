6a451d77afd847061db617e58719d126e796395c
James Shore <jshore@jamesshore.com>
2013-03-07 21:04:45 -0800

    Updated readme and stripped out unneeded Automatopia files

--- Jakefile.js
+++ Jakefile.js
@@ -12,7 +12,6 @@
 	];
 
 	var lint = require("./build/util/lint_runner.js");
-	var nodeunit = require("./build/util/nodeunit_runner.js");
 	var testacular = require("./build/util/testacular_runner.js");
 
 	desc("Lint and test");
@@ -27,41 +26,14 @@
 
 	desc("Lint everything");
 	task("lint", [], function () {
-		var passed = lint.validateFileList(nodeFilesToLint(), nodeLintOptions(), {});
-		passed = lint.validateFileList(browserFilesToLint(), browserLintOptions(), {}) && passed;
+		var passed = lint.validateFileList(browserFilesToLint(), browserLintOptions(), {});
 		if (!passed) fail("Lint failed");
 	});
 
-	desc("Test everything");
-	task("test", ["testServer", "testClient"]);
-
-	desc("Test node.js code");
-	task("testServer", function() {
-		nodeunit.runTests(nodeFilesToTest(), complete, fail);
-	}, {async: true});
-
 	desc("Test browser code");
-	task("testClient", function() {
+	task("test", function() {
 		testacular.runTests(REQUIRED_BROWSERS, complete, fail);
 	}, {async: true});
-
-	function nodeFilesToTest() {
-		var testFiles = new jake.FileList();
-		testFiles.include("src/_*_test.js");
-		testFiles.include("src/server/**/_*_test.js");
-		testFiles.exclude("node_modules");
-		var tests = testFiles.toArray();
-		return tests;
-	}
-
-	function nodeFilesToLint() {
-		var files = new jake.FileList();
-		files.include("src/*.js");
-		files.include("src/server/**/*.js");
-		files.include("build/util/**/*.js");
-		files.include("Jakefile.js");
-		return files.toArray();
-	}
 
 	function browserFilesToLint() {
 		var files = new jake.FileList();
@@ -86,12 +58,6 @@
 			strict:true,
 			trailing:true
 		};
-	}
-
-	function nodeLintOptions() {
-		var options = globalLintOptions();
-		options.node = true;
-		return options;
 	}
 
 	function browserLintOptions() {
